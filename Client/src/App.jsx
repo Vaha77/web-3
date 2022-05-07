813841b47ba527827d3a071f17594874b55fb54e
vaha77 <abdulvohid996@gmail.com>
2022-05-07 16:45:23 +0900

    kd

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,17 +1,14 @@
-import "./App.css";
 import { Navbar, Footer, Welcome, Trasactions, Services } from "./components";
-const App = () => {
-  return (
-    <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
-        <Navbar />
-        <Welcome />
-      </div>
-      <Services />
-      <Trasactions />
-      <Footer />
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
-  );
-};
+    <Services />
+    <Trasactions />
+    <Footer />
+  </div>
+);
 
 export default App;
