5acf3f0706f01b297cba335700aa753ba2eafb03
vaha77 <abdulvohid996@gmail.com>
2022-05-10 07:35:36 +0900

    ls

--- Smart_Contract/hardhat.config.js
+++ Smart_Contract/hardhat.config.js
@@ -1,21 +1,15 @@
+// https://eth-ropsten.alchemyapi.io/v2/_KsGr0W9_gTkxeuOeyZgFjp9qlNxShIW
+
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/_KsGr0W9_gTkxeuOeyZgFjp9qlNxShIW",
+      acaunt: [
+        "d568867f8319ceb6176f2b43f30a88deb432f3343a6c2d07cdec3a5fb5e7088c",
+      ],
+    },
+  },
 };
