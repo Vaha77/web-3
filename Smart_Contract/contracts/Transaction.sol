071441576dabacbd3873a13feecf75eef7607cb9
vaha77 <abdulvohid996@gmail.com>
2022-05-09 17:26:35 +0900

    transactions

---
+++ Smart_Contract/contracts/Transaction.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.0;
+
+contract Transaction {
+
+uint256 transactionCounter;
+
+ event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+
+
+struct TransferStruct {
+    address sender;
+    address receiver;
+    uint amount;
+    string message;
+    uint256 timestamp;
+    string keyword;
+}
+
+TransferStruct[] transaction;
+function addToBlockchain() public{}
+function getAllTransactions() public view returns(TransferStruct[]memory) {
+    // return transaction;
+}
+function getTransactionCount()  public view returns(uint256){}
+// return getTransactionCount;
+}
