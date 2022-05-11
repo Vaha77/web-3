b7512058d93906cad7255d5d5d66589226c95b73
vaha77 <abdulvohid996@gmail.com>
2022-05-11 09:40:18 +0900

    cs

--- Client/src/components/Welcome.jsx
+++ Client/src/components/Welcome.jsx
@@ -3,7 +3,8 @@ import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from ".";
-
+import { TransactionContext } from "../context/TransactionContext";
+import { useContext } from "react";
 const companyCommonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -18,6 +19,8 @@ const Input = ({ placeholder, handleChange, type, name, value }) => (
   />
 );
 const Welcome = () => {
+  const { value } = useContext(TransactionContext);
+
   const handleSubmit = () => {};
   const connectWallet = () => {};
   return (
