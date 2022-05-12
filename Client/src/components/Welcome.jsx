4071e94bc4ac4a6f02f99072e68230755cafa9c0
vaha77 <abdulvohid996@gmail.com>
2022-05-12 18:17:44 +0900

    cs

--- Client/src/components/Welcome.jsx
+++ Client/src/components/Welcome.jsx
@@ -20,7 +20,6 @@ const Input = ({ placeholder, handleChange, type, name, value }) => (
 );
 const Welcome = () => {
   const { value } = useContext(TransactionContext);
-  const { value } = useContext(TransactionContext);
 
   const handleSubmit = () => {};
   const connectWallet = () => {};
