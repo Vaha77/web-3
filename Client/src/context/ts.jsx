3fb312e412677d307065c1e642b1f40f8fd7514c
vaha77 <abdulvohid996@gmail.com>
2022-06-11 09:57:29 +0900

    cs

---
+++ Client/src/context/ts.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { contractABI, contractAddress } from "../utils/constants";
+
+export const TransactionContext = React.createContext();
+
+const { ethereum } = window;
+
+const getEthereumContract = () => {
+  const provider = new ethers.providers.web3provider(ethereum);
+  const signer = provider.getSigner();
+  const transactionContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+
+  console.log({ provider, signer, transactionContract });
+};
+
+export const TransactionsProvider = ({ children }) => {
+  const checkIfWalletIsConnect = async () => {
+    if (!ethereum) return alert("place install metamask");
+
+    const accounts = await ethereum.request({ method: "eth_account" });
+    console.log(accounts);
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnect();
+  }, []);
+
+  return (
+    <TransactionContext.Provider value={{ value: "test" }}>
+      {children}
+    </TransactionContext.Provider>
+  );
+};
